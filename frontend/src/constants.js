0ecd0c8c4c65601bbed4d228e96c42ad7d0172a0
kgkagunat <kgkagunat@gmail.com>
2023-08-21 14:43:15 -1000

    "created constants and store"

---
+++ frontend/src/constants.js
@@ -0,0 +1,8 @@
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : '';
+
+export const PRODUCTS_URL = `/api/products/`;
+export const USERS = `/api/users/`;
+export const ORDERS = `/api/orders/`;
+export const PAYPAL = `/api/config/paypal`;
+
+
